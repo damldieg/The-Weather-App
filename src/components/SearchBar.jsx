2fd1f4803aa27d32d8cc9d723a89cab0e337c6c0
damldieg <damian.diego.2510@gmail.com>
2021-07-29 17:37:24 +0200

    Input fix/Styles changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,13 +9,16 @@ export default function SearchBar({onSearch}) {
     <form className="search" onSubmit={(e) => {
       e.preventDefault();
       onSearch(city);
+      document.getElementById("bar").value = "";
     }}>
       <input
+        id="bar"
         type="text"
         placeholder="Ciudad..."
         className="barra"
-        value={city}
+        //value={city}
         onChange={e => setCity(e.target.value)}
+        autocomplete ="off"
       />
       <button type="submit"  className="boton"><IoSearch/></button>
     </form>
